ebab42858e77193a89bc72a67f6c3752d5066eab
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2022-09-18 00:53:19 -0600

    refactor: still grinding on this notes app but this is a super fun concept to play with

--- src/index.js
+++ src/index.js
@@ -6,39 +6,57 @@ function getLastWord(str) {
   return str.split(' ').pop();
 }
 
-const rhymesSection = document.querySelector(".rhymes");
-const soundalikeSection = document.querySelector(".rhymes-alt");
-
 const api = new Api({url: "https://api.datamuse.com/words"});
 
 const bodyInput = document.querySelector(".form-input-body");
 bodyInput.value = "";
+
+function processApi({data, sectionSelector}) {
+  const section = document.querySelector(sectionSelector);
+  section.innerHTML = "";
+  data.forEach(word => {
+    const rhyme = new Rhyme({
+      word: word.word,
+      templateSelector: "#rhyme",
+      sectionSelector: sectionSelector
+    });
+    rhyme.addRhyme(rhyme.generateRhyme());
+  })
+}
 
 bodyInput.addEventListener("input", (evt) => {
     evt.preventDefault();
     const input = getLastWord(bodyInput.value);
     api.getRhyme(input)
       .then(words => {
-        rhymesSection.innerHTML = "";
-        words.forEach(word => {
-        const rhyme = new Rhyme({
-          word: word.word,
-          templateSelector: "#rhyme",
-          sectionSelector: ".rhymes"
+        processApi({
+          data: words,
+          sectionSelector: ".rhymes-def"
         });
-        rhyme.addRhyme(rhyme.generateRhyme());
-      })});
+      });
     api.getSoundAlike(input)
       .then(words => {
-        soundalikeSection.innerHTML = "";
-        words.forEach(word => {
-          const rhyme = new Rhyme({
-            word: word.word,
-            templateSelector: "#rhyme",
-            sectionSelector: ".rhymes-alt"
-          });
-          rhyme.addRhyme(rhyme.generateRhyme());
-        })
+        processApi({
+          data: words,
+          sectionSelector: ".rhymes-sound"
+        });
       });
-    
+    api.getRelatedWords(input)
+      .then(words => {
+        processApi({
+          data: words,
+          sectionSelector: ".rhymes-rel"
+        });
+      });
+  });
+
+  const buttons = Array.from(document.querySelectorAll(".fa-eye"));
+  buttons.forEach(button => {
+    button.addEventListener("mouseover", () => {
+      button.classList.add("fa-eye-slash");
+    });
+    button.addEventListener("click", () => {
+      const rhymes = button.closest(".rhymes");
+      rhymes.classList.add("rhymes-hidden");
+    });
   })
